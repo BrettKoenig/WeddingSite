645517779dd9d574eb277c0e106a0b87f8e3ddef
Brett Koenig <koenig.337@osu.edu>
2014-11-16 23:47:34 -0500

    Working on rsvp form

--- js/agency.js
+++ js/agency.js
@@ -24,3 +24,46 @@ $('body').scrollspy({
 $('.navbar-collapse ul li a').click(function() {
     $('.navbar-toggle:visible').click();
 });
+
+//Brett function
+
+document.getElementById("RSVPsubmit").onclick = function CheckRsvp() 
+{
+	var guestList = [["Brett Koenig", "Tricia Dresbach"], ["Nick Koenig", "Emily Koenig"]];
+	var counter = 0;
+	var innerCounter = 0;
+	var flag = false;
+	while (counter < guestList.length && !flag)
+	{
+		while (innerCounter < guestList[counter].length && !flag)
+		{
+			if(document.getElementById("RSVPname").value == guestList[counter][innerCounter])
+			{
+				document.getElementById("RSVPname").readOnly = true;
+				flag = true;
+				break;
+			}
+			innerCounter++;
+		}
+		if (flag)
+		{
+			break;
+		}
+		innerCounter = 0;
+		counter++;
+	}
+	if(flag)
+	{
+		var displayString;
+		var displayCounter = 0;
+		displayString = "<ul>";
+		while(displayCounter < guestList[counter].length)
+		{
+			displayString = displayString + "<li>" + guestList[counter][displayCounter] + "<div class=\"radio\"><label><input type=\"radio\" name=\"optionsRadios"+ displayCounter +"\" id=\"optionsRadios1\" value=\"option1\" checked>Yes</label></div><div class=\"radio\"><label><input type=\"radio\" name=\"optionsRadios"+ displayCounter +"\" id=\"optionsRadios2\" value=\"option2\">No</label></div></li>";
+			displayCounter++;
+		}
+		displayString = displayString + "</ul>";
+		document.getElementById("displayNames").innerHTML = displayString;
+		document.getElementById("displayNames").style.display = "block";
+	}
+};
